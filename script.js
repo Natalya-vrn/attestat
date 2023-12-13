7f043524ebe97b486fe204eef250c3764e3b7a14
Natalya-vrn <n.akimova-vrn@mail.ru>
2023-12-13 04:00:13 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function submitForm() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let message = document.getElementById('message').value;
+
+    document.getElementById('contactForm').reset();
+}
